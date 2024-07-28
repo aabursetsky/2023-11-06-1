4cf7f5f9c447529b40e4b117e7b79c283f6c7c34
aabursetsky <114884761+aabursetsky@users.noreply.github.com>
2024-07-28 18:40:08 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,53 @@
+class Horse:
+    x_distance = 0
+
+    def __init__(self):
+        self.sound = 'Frrr'
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:
+    y_distance = 0
+
+    def __init__(self):
+        self.sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus (Horse, Eagle):
+    def __init__(self):
+        super().__init__()
+        Eagle.__init__(self)           # Horse.sound change to Eagle.sound :)
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        print(self.sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
+
+
+'''
+(0, 0)
+(10, 15)
+(5, 35)
+I train, eat, sleep, and repeat
+'''
